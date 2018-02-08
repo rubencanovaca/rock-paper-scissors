947c824a1ca9f391608f4209cf4081eae7f854a9
ruben.canovaca.nieto <ruben.canovaca.nieto@accenture.com>
2018-02-08 18:47:50 +0100

    Add bootstrap 4

--- src/app/components/combat/combat.component.ts
+++ src/app/components/combat/combat.component.ts
@@ -13,14 +13,32 @@ import * as _ from 'lodash';
 export class CombatComponent implements OnInit {
     @Input() players: Player;
     weapons = WEAPONS;
+    imgUrl = '../../../assets/images';
+    game = {
+        selected: false,
+        countdown: 3,
+        computerChoice: 0,
+        winner: 0,
+        over: false
+    };
 
     getImage = function(weapon: Weapon, player: Player) {
-        return `<img src="../../../assets/images/${weapon.name.toLowerCase()}_player${player.id}.png" alt="${weapon.name}" />`;
+        return `<img class="img-fluid" src="${this.imgUrl}/${weapon.name.toLowerCase()}_player${player.id}.png" alt="${weapon.name}" />`;
     };
 
     onSelect(weapon: Weapon): void {
-        _.each(this.weapons, (w) => { w.selected = false; });
-        weapon.selected = true;
+        if (!this.game.over) {
+            _.each(this.weapons, (w) => {
+                w.selected = false;
+            });
+            weapon.selected = true;
+            this.game.selected = true;
+        }
+    }
+
+    onFight() {
+        this.game.computerChoice = 1;
+        this.game.over = true;
     }
 
     constructor() {
